c8c7c53769c6b8bc91fb55e3731cbbf43fca29af
帝少 <dishao@msn.com>
2024-12-28 01:35:26 +0800

    Add files via upload

---
+++ static/assets/headings_id.js
@@ -0,0 +1,14 @@
+﻿function generateHeadingIds() {
+    var contentContainer = document.getElementById('content');
+    const headings = contentContainer.querySelectorAll('h1, h2, h3, h4, h5, h6');
+    
+    headings.forEach(heading => {
+        if (!heading.id) {
+            heading.id = heading.textContent.trim().replace(/\s+/g, '-').toLowerCase();
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    generateHeadingIds();
+});
